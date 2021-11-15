bb21e41f30fbc17f2fde1560c2c398ed7c2bea4b
Henry Aybar <aybar.henry.usmc@gmail.com>
2021-11-14 22:26:09 -0600

    Inital commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,45 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios'
+import {BrowserRouter, Link, Switch, Route} from 'react-router-dom'
+
 
 function App() {
+
+  const [resData, setResData] = useState([])
+  
+  const fetchLuke = () =>{
+    axios.get('https://swapi.dev/api/planets/1/')
+      .then(res=>{ setResData(res.data) })
+      console.log(resData)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter className="App">
+    <header className="App-header">
+      <h1>Routing fun</h1>
+      <Link to='/'>Home</Link>
+      <Link to='/bob'>Bobs</Link>
+    </header>
+
+    <button onClick={ fetchLuke }> Submit </button>
+    {/* <div>
+      { "" + resData }
+    </div> */}
+
+    <Switch>
+      <Route exact path='/'>
+        <p>Home</p>
+      </Route>
+
+      <Route exact path='/bob'>
+        <p>Bob's Page</p>
+      </Route>
+
+    </Switch>
+
+  </BrowserRouter>
   );
 }
 
